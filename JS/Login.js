73b85fe9caa6ef89fcb5a6b0f4ab46b2ddece5d0
HungHia198 <levanhung.1982004@gmail.com>
2024-06-06 21:12:52 +0700

    None

--- JS/Login.js
+++
@@ -1,7 +0,0 @@
-function signIn() {
-    // Xử lý đăng nhập tại đây
-
-    alert("Đăng nhập thành công!");
-    window.location.href = 'C:\Users\Admin\OneDrive\Máy tính\FF\ASM\HTML\Home\Home.html';
-}
-
